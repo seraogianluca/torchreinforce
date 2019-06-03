de1fde8792b21f21f5d8f36782ca8789229eeceb
Gianluca Serao <gianluca.serao.dj@gmail.com>
2019-06-03 14:41:04 +0200

    Commit

--- examples/mountaincar.py
+++ examples/mountaincar.py
@@ -35,13 +35,7 @@ class Policy(DeepReinforceModule):
 target_net = Policy(gamma=0.99, epsilon_init=0.9, epsilon_max=0.1, epsilon_decay=0.99)
 policy_net = Policy(gamma=0.99, epsilon_init=0.9, epsilon_max=0.1, epsilon_decay=0.99, target_net=target_net)
 
-#Weights initializer
-def init_normal(m):
-    if type(m) == nn.Linear:
-        nn.init.uniform_(m.weight)
 
-policy_net.apply(init_normal)
-policy_net.target_net.apply(init_normal)
 
 for each_game in range(1000):
     total_reward = 0
@@ -52,10 +46,10 @@ for each_game in range(1000):
         next_state, reward, done, _ = env.step(action)
         next_state = torch.as_tensor(next_state, dtype=torch.float, device=device)
 
-        if next_state[0].item() > -0.5:
-            reward = 1.
+        #if next_state[0].item() > -0.5:
+        #    reward = 1.
 
-        reward = numpy.clip(reward, -1, 1)
+        #reward = numpy.clip(reward, -1, 1)
 
         if done:
             next_state = None
